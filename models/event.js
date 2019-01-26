f298dc4c4ac0a3b8339c6ab3ed04b3371b0b0e8d
Raion314 <raion314@outlook.com>
2019-01-25 19:51:49 -0800

    add user reference

--- models/event.js
+++ models/event.js
@@ -5,7 +5,8 @@ const eventSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
-  date: { type: Date, required: true }
+  date: { type: Date, required: true },
+  createdBy: { type: Schema.Types.ObjectId, ref: "User" }
 });
 
 module.exports = mongoose.model("Event", eventSchema);
