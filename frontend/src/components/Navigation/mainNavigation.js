5c58ba0828045d41b162b0b430919874e9d47a33
Raion314 <raion314@outlook.com>
2019-01-27 12:42:39 -0800

    add mainNavigation component

---
+++ frontend/src/components/Navigation/mainNavigation.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./mainNavigation.css";
+
+const mainNavigation = props => (
+  <header className="main-navigation__header">
+    <div className="main-navigation__logo">
+      <h1>Booking Events App</h1>
+    </div>
+    <navbar className="main-navigation__items">
+      <ul>
+        <li>
+          <NavLink to="/auth">Sign In</NavLink>
+        </li>
+        <li>
+          <NavLink to="/events">Events</NavLink>
+        </li>
+        <li>
+          <NavLink to="/bookings">Events</NavLink>
+        </li>
+      </ul>
+    </navbar>
+  </header>
+);
+
+export default mainNavigation;
