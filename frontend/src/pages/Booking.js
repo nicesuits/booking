33d1d332be98a15a04dd45a0d5931475dc70206d
Raion314 <raion314@outlook.com>
2019-01-27 11:27:09 -0800

    initial commit

---
+++ frontend/src/pages/Booking.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class BookingPage extends Component {
+  render() {
+    return <h1>The BookingPage</h1>;
+  }
+}
+
+export default BookingPage;
---
+++ frontend/src/pages/User.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class UserPage extends Component {
+  render() {
+    return <h1>The UserPage</h1>;
+  }
+}
+
+export default UserPage;
