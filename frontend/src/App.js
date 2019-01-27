c124fd96bebdb14cf86e1d8cf3bc8ae2521c481f
Raion314 <raion314@outlook.com>
2019-01-27 11:26:38 -0800

    add routing to main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import "./App.css";
+
+import AuthPage from "./pages/Auth";
+import BookingPage from "./pages/Booking";
+import EventPage from "./pages/Event";
+import UserPage from "./pages/User";
 
 class App extends Component {
   render() {
-    return <div className="App">hello world</div>;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Redirect from="/" to="auth" exact />
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/bookings" component={BookingPage} />
+          <Route path="/events" component={EventPage} />
+          <Route path="/user" component={UserPage} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
